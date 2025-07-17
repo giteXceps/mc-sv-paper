860c8adcaf55215d29ecffc47475b216b9c129d6
Sefa Metin <sefametindev@gmail.com>
2025-07-17 14:20:13 +0300

    start.sh screen eklentisi

--- start.sh
+++ start.sh
@@ -1,1 +1,11 @@
-java -Xmx4G -Xms1G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=50 -jar paper.jar nogui
+# Ekran adı (istediğini verebilirsin)
+SCREEN_NAME="mc"
+
+# Screen zaten açık mı kontrol et
+if screen -list | grep -q "$SCREEN_NAME"; then
+  echo "Screen '$SCREEN_NAME' zaten açık."
+else
+  echo "Screen '$SCREEN_NAME' başlatılıyor..."
+  screen -dmS $SCREEN_NAME java -Xmx5G -Xms1G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=50 -jar paper.jar nogui
+  echo "Sunucu '$SCREEN_NAME' içinde başlatıldı."
+fi
