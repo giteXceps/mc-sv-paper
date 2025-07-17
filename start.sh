f96d6dd389a752d95577eee0bb7dac83b5b0a1b5
Sefa Metin <sefametindev@gmail.com>
2025-07-17 14:34:36 +0300

    start.sh ile optimizasyon

--- start.sh
+++ start.sh
@@ -6,6 +6,6 @@ if screen -list | grep -q "$SCREEN_NAME"; then
   echo "Screen '$SCREEN_NAME' zaten açık."
 else
   echo "Screen '$SCREEN_NAME' başlatılıyor..."
-  screen -dmS $SCREEN_NAME java -Xmx5G -Xms1G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=50 -jar paper.jar nogui
+  screen -dmS $SCREEN_NAME java -Xms2G -Xmx5G -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:G1NewSizePercent=20 -XX:G1ReservePercent=20 -XX:MaxGCPauseMillis=50 -XX:+ParallelRefProcEnabled -jar paper.jar nogui
   echo "Sunucu '$SCREEN_NAME' içinde başlatıldı."
 fi
