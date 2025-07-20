aaa9ec31728d059ac9420cc408a08a1733f2dcba
Sefa Metin <sefametindev@gmail.com>
2025-07-20 05:26:00 +0300

    start.sh düzeltme

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Ekran adı (istediğini verebilirsin)
 SCREEN_NAME="mc"
 
@@ -6,6 +8,6 @@ if screen -list | grep -q "$SCREEN_NAME"; then
   echo "Screen '$SCREEN_NAME' zaten açık."
 else
   echo "Screen '$SCREEN_NAME' başlatılıyor..."
-  screen -dmS $SCREEN_NAME java -Xms2G -Xmx5G -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:G1NewSizePercent=20 -XX:G1ReservePercent=20 -XX:MaxGCPauseMillis=50 -XX:+ParallelRefProcEnabled -jar paper.jar nogui
+  screen -dmS $SCREEN_NAME bash -c 'while true; do java -Xms2G -Xmx5G -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:G1NewSizePercent=20 -XX:G1ReservePercent=20 -XX:MaxGCPauseMillis=50 -XX:+ParallelRefProcEnabled -jar paper.jar nogui; echo "Sunucu kapandı, 5 saniye sonra yeniden başlıyor..."; sleep 5; done'
   echo "Sunucu '$SCREEN_NAME' içinde başlatıldı."
 fi
